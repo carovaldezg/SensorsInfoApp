216e3a24620e95f80ff8dff5f1f54d9fbdf1cd98
carovaldezg <caro.valdezg@gmail.com>
2016-12-28 14:44:38 -0300

    first commit sensors app

---
+++ app/src/main/java/com/example/caro/sensorsinfoapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.caro.sensorsinfoapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class MainActivity extends AppCompatActivity {
+
+    SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView sensorTextView = (TextView)findViewById(R.id.sensorInfoTextView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> mList= sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (int i = 1; i < mList.size(); i++) {
+            sensorTextView.setVisibility(View.VISIBLE);
+            sensorTextView.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
+    }
+
+}
