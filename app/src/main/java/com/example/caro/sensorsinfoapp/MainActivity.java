bc6d43188325f57c889e948f1c8c71d883b0b52e
carovaldezg <caro.valdezg@gmail.com>
2017-01-05 20:00:53 -0300

    listview layout

--- app/src/main/java/com/example/caro/sensorsinfoapp/MainActivity.java
+++ app/src/main/java/com/example/caro/sensorsinfoapp/MainActivity.java
@@ -5,8 +5,11 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
@@ -20,15 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView sensorTextView = (TextView)findViewById(R.id.sensorInfoTextView);
+        ListView listaSensores = (ListView)findViewById(R.id.sensorsListView);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> mList= sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        for (int i = 1; i < mList.size(); i++) {
-            sensorTextView.setVisibility(View.VISIBLE);
-            sensorTextView.append("\n Nombre: " + mList.get(i).getName() + "\n Vendor: " + mList.get(i).getVendor() + "\n version: " + mList.get(i).getVersion()+ "\n");
-        }
+        ArrayList<String> sensorsinfo = new ArrayList<String>();
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.sensorlist_item, R.id.textView ,mList);
+        listaSensores.setAdapter(adapter);
+
+
+
     }
 
 }
